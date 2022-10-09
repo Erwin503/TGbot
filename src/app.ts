a2484b326c2351c0bae9608c19f411c452a9f3aa
Chelovek A <cheloveka@Cheloveks-MacBook-Pro.local>
2022-10-09 18:22:00 +0300

    created simple example

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("Hello World!")
+import {Telegraf} from 'telegraf'
+
+const bot = new Telegraf("5399924741:AAGz4h0IRaamBk6UqVbIiLi4qzybJ1X_hO0");
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('пошёл в жопу'));
+bot.hears('hi', (ctx) => ctx.reply('hey'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
