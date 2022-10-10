a57276fb49c3ba8022172a0c793e245741624ed9
Chelovek A <cheloveka@Cheloveks-MacBook-Pro.local>
2022-10-10 18:22:30 +0300

    connect to googlesheets

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,45 @@
+import { assert, Console } from 'console';
 import {Telegraf} from 'telegraf'
+import {GoogleSpreadsheet} from 'google-spreadsheet'
+require('dotenv').config()
 
-const bot = new Telegraf("5399924741:AAGz4h0IRaamBk6UqVbIiLi4qzybJ1X_hO0");
+const tgToken = process.env["TG_BOT_TOKEN"]
+
+assert(tgToken != null, "No TG_BOT_TOKEN enviroment variable found")
+const bot = new Telegraf(tgToken as string);
+
+const doc = new GoogleSpreadsheet("1ctJQZHqifcDq5BZMkhnmsHeBdirapieEG78zk-nbi4A/edit#gid=0")
+
+const mgrEmail = process.env["MANAGER_EMAIL"]
+
+const mgrKey = process.env["MANAGER_KEY"]
+
+assert(mgrEmail != null, "No MANAGER_EMAIL enviroment variable found")
+assert(mgrKey != null, "No MANAGER_KEY enviroment variable found")
+
+// console.log(tgToken)
+// console.log(mgrKey)
+// console.log(mgrEmail)
+
+start()
+
+async function start() {
+try {
+    await doc.useServiceAccountAuth({client_email: mgrEmail!,
+         private_key: mgrKey!.replace(/\\n/g, '\n')})
+
+} catch (e) {
+    console.error(e)
+}
+
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('пошёл в жопу'));
-bot.hears('hi', (ctx) => ctx.reply('hey'));
+bot.on('text', (ctx) => {
+    ctx.reply(ctx.message)
+    console.info(ctx.message)
+})
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on('sticker', (ctx) => ctx.reply('больше не пиши сюда.'));
+// bot.hears('hi', (ctx) => ctx.reply('hey'));
 bot.launch();
+}
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
